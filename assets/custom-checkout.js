d0cc6c12bb8da88c456badb0e8272c8f842463bc
datng-nfq <128581248+datng-nfq@users.noreply.github.com>
2023-04-25 11:16:53 +0700

    feat(OCI-755): enhancement countdown (#287)
    * feat(OCI-755): enhancement countdown
    
    * feat(OCI-755):  stop interval when countdown finish
    
    * feat(OCI-755): fix spelling
    
    * feat(OCI-755): fix spelling
    
    * feat(OCI-755): Fix spelling
    
    * feat(OCI-755): remove save countdown time before load page
    
    ---------
    
    Co-authored-by: datnguyen-nfq <dat.nguyen@nfq.com>

--- assets/custom-checkout.js
+++ assets/custom-checkout.js
@@ -18,6 +18,29 @@ let CustomCheckout = {
   },
 };
 
+let CountDown = { 
+  saveCountDown: function() { 
+    const timer = document.getElementById('timer');
+    if (timer && timer.innerHTML !== '00:00') { 
+      sessionStorage.setItem('countdown', timer.innerHTML);
+    } 
+    if (countdownInterVal) { 
+      clearInterval(countdownInterVal);
+    }
+  },
+  
+  init: function() { 
+    const continueBtn = document.getElementById('continue_button');
+    const breadcrumbLinks = document.getElementsByClassName('breadcrumb__link');
+
+    for (var i = 0; i < breadcrumbLinks.length; i++) {
+      breadcrumbLinks[i].addEventListener('click', this.saveCountDown, false);
+    }
+
+    continueBtn.addEventListener('click', this.saveCountDown, false);
+  }
+};
+
 // Init mutationObserver to watch for changes in the DOM
 let mutationObserver = new MutationObserver(function() {
   mutationObserver.disconnect();
@@ -36,4 +59,5 @@ $(document).on("click", "#checkout_submit, .tag__button", function() {
 
 $(document).ready(function() {
   CustomCheckout.init();
+  CountDown.init();
 });
