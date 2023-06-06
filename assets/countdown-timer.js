40dc58d107152f22fc62de93fb01d13864851c27
trucht-emma <100551442+trucht-emma@users.noreply.github.com>
2023-06-06 14:02:09 +0700

    [OCI-863] 3 messages on the sticking banner to main (#336)
    * Update datetime labels with lang check
    
    * OCI-863: 3 message on the sticking banner (#320)
    
    * OCI-863: 3 message on the sticking banner - allow 1 slide only (#321)
    
    * announcement bar feedbacks (#326)
    
    * nnouncement bar feedbacks (#327)
    
    * Update announcement bar

---
+++ assets/countdown-timer.js
@@ -0,0 +1,51 @@
+class Countdown {
+ constructor(expiredDate, onRender, onComplete) {
+  this.setExpiredDate(expiredDate);
+
+  this.onRender = onRender;
+  this.onComplete = onComplete;
+ }
+
+ setExpiredDate(expiredDate) {
+  const currentTime = new Date().getTime();
+
+  this.timeRemaining = expiredDate - currentTime;
+
+  this.timeRemaining > 0 ? this.start() : this.complete();
+ }
+
+ complete() {
+  if (typeof this.onComplete === 'function') {
+   this.onComplete();
+  }
+ }
+
+ start() {
+  this.update();
+
+  const intervalId = setInterval(() => {
+   this.timeRemaining -= 1000;
+   if (this.timeRemaining < 0) {
+    this.complete();
+    clearInterval(intervalId);
+   } else {
+    this.update();
+   }
+  }, 1000);
+ }
+
+ getTime() {
+  return {
+   days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
+   hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
+   minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
+   seconds: Math.floor(this.timeRemaining / 1000) % 60
+  };
+ }
+
+ update() {
+  if (typeof this.onRender === 'function') {
+   this.onRender(this.getTime());
+  }
+ }
+}
