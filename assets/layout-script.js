255599b1b7d171dad66fa48ac21a5caa7402633c
Jack <hieu.huynh@jazurite.com>
2024-02-16 21:47:59 +0700

    Merge Shopify Live February 16, 2024

--- assets/layout-script.js
+++ assets/layout-script.js
@@ -35,9 +35,9 @@ $(document).ready(function () {
   })
 }
 function freshChat() {
-  var fc_JS=document.createElement('warp-script');
-  fc_JS.setAttribute("src", 'https://wchat.eu.freshchat.com/js/widget.js?t='+Date.now());
-  fc_JS.setAttribute("loading", "delayed");
+  var fc_JS=document.createElement('script', {is: "delay-script"});
+  fc_JS.setAttribute("url", 'https://wchat.eu.freshchat.com/js/widget.js?t='+Date.now());
+  fc_JS.setAttribute("phase", "interactive");
 
   (document.body?document.body:document.getElementsByTagName('head')[0]).appendChild(fc_JS);
   window.fcSettings = {
