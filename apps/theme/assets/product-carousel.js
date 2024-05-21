95225484aae9ade136e330cffcb24d2b732c4d32
Jack <hieu.huynh@jazurite.com>
2024-05-21 17:44:01 +0700

    hotfix: Resolve issue broken layout Product Carousel

--- apps/theme/assets/product-carousel.js
+++ apps/theme/assets/product-carousel.js
@@ -1,20 +1,30 @@
 $("#product-carousel-section").ready(async function () {
-    await ResourceCoordinator.requestVendor('Splide');
+  await ResourceCoordinator.requestVendor("Splide");
 
-    new Splide('#product-carousel-section .splide', {
-        gap: '8px',
-        padding: {right: '50'},
-        autoWidth: true,
-        mediaQuery: 'min',
-        arrows: false,
-        pagination: true,
-        breakpoints: {
-            768: {
-                perPage: 4,
-                padding: false,
-                arrows: true,
-                gap: '32px'
-            },
-        },
-    }).mount();
+  new Splide("#product-carousel-section .splide", {
+    gap: ".5rem",
+    padding: { right: 50 },
+    mediaQuery: "min",
+    arrows: false,
+    pagination: true,
+    breakpoints: {
+      640: {
+        perPage: 1,
+        perMove: 1,
+        padding: false,
+        arrows: true,
+      },
+      768: {
+        perPage: 2,
+        gap: "1rem",
+      },
+      840: {
+        perPage: 3,
+      },
+      1200: {
+        perPage: 4,
+        gap: "1.5rem",
+      }
+    },
+  }).mount();
 });
