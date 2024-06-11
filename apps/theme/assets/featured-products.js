fbc2fdfad0e27e458162f8d7b3598b40c4f080f1
Jack <hieu.huynh@jazurite.com>
2024-06-11 17:21:22 +0700

    hotfix: Resolve issue broken Splide

--- apps/theme/assets/featured-products.js
+++ apps/theme/assets/featured-products.js
@@ -1,22 +1,22 @@
 $("#featured-products-section").ready(async () => {
-    await ResourceCoordinator.requestVendor('Splide');
+  await ResourceCoordinator.requestVendor("Splide");
 
-    new Splide('#featured-products-section .splide', {
-        autoWidth: false,
-        padding: {right: '0rem'},
-        perPage: 4,
-        perMove: 1,
-        gap: '20px',
+  new Splide("#featured-products-section .splide", {
+    autoWidth: false,
+    padding: { right: "0rem" },
+    perPage: 4,
+    perMove: 1,
+    gap: "20px",
+    pagination: true,
+    arrows: false,
+    breakpoints: {
+      881: {
+        autoWidth: true,
+        perPage: 1,
+        arrows: false,
         pagination: true,
-      arrows:false,
-        breakpoints: {
-            881: {
-               autoWidth: true,
-                perPage: 1,
-                arrows: false,
-                pagination: true,
-                gap: '12px',
-            }
-        }
-    }).mount();
-})
+        gap: "12px",
+      },
+    },
+  }).mount();
+});
