839807a27873ab33d79fb72bc74ee3a62c5b3185
Jack <hieu.huynh@jazurite.com>
2024-06-19 14:57:02 +0700

    feat<Collections>: Integrate with custom tagging

--- apps/theme/assets/sale-catalog.js
+++ apps/theme/assets/sale-catalog.js
@@ -2,7 +2,7 @@
     const $smartTabs = $("#sale-catalog-section smart-tabs")
     console.log("=>(sale-catalog.js:3) $smartTabs", $smartTabs);
 
-    const $tabLabels = $smartTabs.find(".smart-tab-label-text-wrapper")
+    const $tabLabels = $smartTabs.find(".smart-tab-label-container")
     const $tabItems = $smartTabs.find("smart-tab-item")
 
     $tabItems.each(function (i) {
