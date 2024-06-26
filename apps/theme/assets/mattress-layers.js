a7d64c26d3521c45f1ca98b23070673dd4c57746
hieu-huynh-emma <100395195+hieu-huynh-emma@users.noreply.github.com>
2024-06-26 11:21:46 +0700

    feat<MattressLayers>: Layers Content Accordion (#430)
    Co-authored-by: jack <hieu.huynh@jazurite.com>

--- apps/theme/assets/mattress-layers.js
+++ apps/theme/assets/mattress-layers.js
@@ -1,4 +1,3 @@
-
 class MattressLayers extends CustomElement {
   constructor() {
     super();
@@ -7,34 +6,37 @@ class MattressLayers extends CustomElement {
   mounted() {
     super.mounted();
 
-    const $layerSpecs = this.$el.find(".layer-info-item");
+    this.init();
+
+    const $layerSpecs = this.$el.find(".layer-spec");
     const $internalLayers = this.$el.find(".internal-layer-item");
     const $coverLayer = this.$el.find(".cover-layer-item");
     const $baseLayer = this.$el.find(".base-layer-item");
     const $outerContainer = this.$el.find(".outer-container");
 
-    $coverLayer.click(function() {
+    $coverLayer.click(function () {
       $(this).addClass("active move-up");
       $outerContainer.removeClass("move-up");
       $internalLayers.removeClass("move-up move-down active");
       $baseLayer.removeClass("active");
-      $layerSpecs.addClass("hidden");
-      $layerSpecs.filter(".cover-layer").removeClass("hidden");
+
+
+      $layerSpecs.filter(`[data-layer-index="${$(this).data("layerIndex")}"]:not(.is-open)`).find(".spec-header").trigger("click");
     });
-    $internalLayers.each(function(i) {
+    $internalLayers.each(function (i) {
       const $layer = $(this);
       $layer.click(() => {
         $layer.removeClass("move-up move-down").addClass("active");
 
-        $layer.siblings().each(function() {
+        $layer.siblings().each(function () {
           $(this).removeClass("active move-up move-down");
         });
 
         $outerContainer.addClass("move-up");
         $baseLayer.removeClass("active");
 
-        $layerSpecs.addClass("hidden");
-        $layerSpecs.eq(i + 1).removeClass("hidden");
+        // $layerSpecs.addClass("hidden");
+        $layerSpecs.filter(`[data-layer-index="${$(this).data("layerIndex")}"]:not(.is-open)`).find(".spec-header").trigger("click");
 
         $layer.prevAll().addClass("move-up");
 
@@ -45,17 +47,36 @@ class MattressLayers extends CustomElement {
         }
       });
     });
-    $baseLayer.click(function() {
+    $baseLayer.click(function () {
       $(this).addClass("active");
       $outerContainer.addClass("move-up");
       $coverLayer.removeClass("active");
       $internalLayers.removeClass("move-up move-down active");
 
-      $layerSpecs.addClass("hidden");
-      $layerSpecs.filter(".base-layer").removeClass("hidden");
+      // $layerSpecs.addClass("hidden");
+      $layerSpecs.filter(`[data-layer-index="${$(this).data("layerIndex")}"]:not(.is-open)`).find(".spec-header").trigger("click");
     });
 
-    $coverLayer.trigger("click");
+    setTimeout(() => {
+      $coverLayer.trigger("click");
+    }, 100)
+  }
+
+  async init() {
+    await ResourceCoordinator.requestVendor("Beefup");
+
+    const $layerItems = $(".layer-item");
+
+    $("#mattress-layers-section .layer-spec").beefup({
+      trigger: ".spec-header",
+      content: ".spec-content",
+      openSpeed: 500,
+      closeSpeed: 250,
+      openSingle: true,
+      onOpen: function ($this) {
+        $layerItems.filter(`[data-layer-index="${$this.data("layerIndex")}"]:not(.active)`).trigger("click");
+      },
+    });
   }
 }
 
