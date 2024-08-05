4137eeb42d552158965528aa9120cbc0ff24c578
Hieu Huynh <trunghieu.huynh@emma-sleep.com>
2024-08-05 14:50:44 +0700

    feat: Build 'Compare At Price' Extension

---
+++ apps/checkout-customizations/extensions/compare-at-price/src/CompareAtPrice.tsx
@@ -0,0 +1,137 @@
+import {
+    reactExtension,
+    BlockStack,
+    Text,
+    useApi,
+    useCartLineTarget,
+    InlineLayout,
+    BlockLayout,
+    View, useSettings, Appearance, InlineAlignment
+} from "@shopify/ui-extensions-react/checkout";
+import {useEffect, useState} from "react";
+import {TextSize} from "@shopify/ui-extensions/build/ts/surfaces/checkout/components/shared";
+import {ExtensionSettings} from "@shopify/ui-extensions/build/ts/surfaces/checkout/api/standard/standard";
+import {TextAccessibilityRole} from "@shopify/ui-extensions/build/ts/surfaces/admin/components/shared";
+
+// 1. Choose an extension target
+export default reactExtension("purchase.checkout.cart-line-item.render-after", () => (
+    <Extension/>
+));
+
+type TextColor = Extract<Appearance, | 'accent' | 'subdued' | 'info' | 'success' | 'warning' | 'critical' | 'decorative'>
+
+async function getLineItem({cartLine, query}) {
+    const {merchandise: {id}, quantity} = cartLine
+
+    const {data, errors} = await query(
+        `query ($id: ID!) {
+         node(id: $id) {
+        ... on ProductVariant {
+            id
+            title
+            quantityAvailable
+            price {
+                amount
+            }
+            compareAtPrice {
+                amount
+            }
+        }
+    }
+      }`,
+        {
+            variables: {id},
+        },
+    )
+
+    const originalPrice = data?.node?.compareAtPrice.amount ?? 0
+
+    cartLine.cost.compareAtPrice = {amount: originalPrice * quantity}
+
+    return cartLine
+}
+
+function DiscountTag(props) {
+    if (!props || !props?.originalPrice) return ""
+
+    const {originalPrice, price, appearance, fontSize} = props
+
+    const diff = originalPrice - price
+    const percent = (diff * 100) / originalPrice
+    const formattedPercent = Math.floor(percent)
+
+    return (
+        <View background={"subdued"} padding={"extraTight"} borderRadius={"base"}>
+            <Text
+                appearance={appearance}
+                size={fontSize}
+            >
+                {formattedPercent}% OFF
+            </Text>
+        </View>
+    )
+}
+
+function getSettings(extensionSettings: Partial<ExtensionSettings>) {
+    const appearance = (extensionSettings.apperance || "subdued") as TextColor,
+        inlineAlignment: InlineAlignment = extensionSettings.alignment === "right" ? "end" : "start",
+        fontSize = (extensionSettings.fontSize || "base") as TextSize,
+        content = extensionSettings.message || "On sale. Originally",
+        strikethrough: TextAccessibilityRole | undefined = extensionSettings.isShowStrikethrough ? "deletion" : undefined,
+        isShowPercentage = extensionSettings.isShowPercentage;
+
+    return {appearance, inlineAlignment, fontSize, content, strikethrough, isShowPercentage};
+}
+
+function Extension() {
+    const settings = getSettings(useSettings())
+    const {i18n, extension, query} = useApi();
+    const cartLine = useCartLineTarget();
+
+    const [pricing, setPricing] = useState({});
+
+    useEffect(async () => {
+        const lineItem = await getLineItem({cartLine, query});
+
+        const {totalAmount: {amount: price}, compareAtPrice: {amount: originalPrice}} = lineItem.cost;
+
+        const totalDiscounts = Math.max(0, originalPrice - price),
+            priceCurrency = i18n.formatCurrency(price),
+            originalPriceCurrency = i18n.formatCurrency(originalPrice),
+            totalDiscountsCurrency = i18n.formatCurrency(totalDiscounts);
+
+        setPricing({
+            price,
+            priceCurrency,
+            originalPrice,
+            originalPriceCurrency,
+            totalDiscounts,
+            totalDiscountsCurrency
+        })
+    }, [query]);
+
+    const {appearance, inlineAlignment, fontSize, content, strikethrough, isShowPercentage} = settings
+
+    if (!pricing.totalDiscounts) return ""
+
+    return isShowPercentage ? <DiscountTag {...pricing} {...settings} /> : (
+        <InlineLayout columns={"auto"} spacing="none" inlineAlignment={inlineAlignment} blockAlignment={"center"}>
+            {content ? (
+                <Text
+                    appearance={appearance}
+                    size={fontSize}
+                >
+                    {content}
+                    &nbsp;
+                </Text>
+            ) : ""}
+
+            <Text appearance={appearance}
+                  accessibilityRole={strikethrough}
+                  size={fontSize}
+            >
+                {pricing?.originalPriceCurrency}
+            </Text>
+        </InlineLayout>
+    );
+}
--- apps/checkout-customizations/extensions/price-breakdown/src/Checkout.tsx
+++ apps/checkout-customizations/extensions/price-breakdown/src/Checkout.tsx
@@ -6,9 +6,6 @@ import {
 } from '@shopify/ui-extensions-react/checkout';
 import {useEffect, useState} from 'react';
 
-export const cartLineItemBreakdown = reactExtension("purchase.checkout.cart-line-item.render-after", () =>
-    <LineItemPriceBreakdown/>);
-
 export const priceBreakdownTop = reactExtension(
     "purchase.checkout.reductions.render-after",
     () => <PriceBreakdownTop/>,
@@ -45,7 +42,7 @@ async function getOrderPriceBreakdown({cartLines, query, i18n}) {
             originalPrice += +originalPriceAmount
         });
 
-        const totalDiscounts = originalPrice - price
+        const totalDiscounts = Math.max(0, originalPrice - price);
 
         const priceCurrency = i18n.formatCurrency(price),
             originalPriceCurrency = i18n.formatCurrency(originalPrice),
@@ -64,8 +61,7 @@ async function getOrderPriceBreakdown({cartLines, query, i18n}) {
 }
 
 async function getLineItem({cartLine, query}) {
-    const {merchandise: {id}} = cartLine
-
+    const {merchandise: {id}, quantity} = cartLine
 
     const {data, errors} = await query(
         `query ($id: ID!) {
@@ -88,54 +84,11 @@ async function getLineItem({cartLine, query}) {
         },
     )
 
-    cartLine.cost.compareAtPrice = data?.node?.compareAtPrice
+    const originalPrice = data?.node?.compareAtPrice.amount ?? 0
+
+    cartLine.cost.compareAtPrice = {amount: originalPrice * quantity}
 
     return cartLine
-}
-
-function LineItemPriceBreakdown() {
-    const {i18n, extension, query} = useApi();
-    const cartLine = useCartLineTarget();
-
-
-    const [pricing, setPricing] = useState({});
-
-    useEffect(async () => {
-        const lineItem = await getLineItem({cartLine, query});
-
-        const {totalAmount: {amount: price}, compareAtPrice: {amount: originalPrice}} = lineItem.cost;
-
-        const totalDiscounts = originalPrice - price,
-            priceCurrency = i18n.formatCurrency(price),
-            originalPriceCurrency = i18n.formatCurrency(originalPrice),
-            totalDiscountsCurrency = i18n.formatCurrency(totalDiscounts);
-
-        setPricing({
-            price,
-            priceCurrency,
-            originalPrice,
-            originalPriceCurrency,
-            totalDiscounts,
-            totalDiscountsCurrency
-        })
-    }, [query]);
-
-    return (
-        <BlockStack spacing="none">
-            <InlineLayout columns={['fill', 'auto']}>
-                <View padding="none" visibility="hidden"></View>
-                <View padding="none" inlineAlignment="end">
-                    <BlockLayout rows={['fill', 'fill']}>
-                        <View>
-                            <Text appearance="subdued"
-                                  accessibilityRole="deletion">{pricing?.originalPriceCurrency}</Text>
-                        </View>
-                        <View></View>
-                    </BlockLayout>
-                </View>
-            </InlineLayout>
-        </BlockStack>
-    );
 }
 
 function PriceBreakdownTop() {
@@ -146,7 +99,6 @@ function PriceBreakdownTop() {
 
     useEffect(async () => {
         const priceBreakdown = await getOrderPriceBreakdown({cartLines, query, i18n});
-        console.log("=>(Checkout.tsx:144) priceBreakdown", priceBreakdown);
 
         setPricing(priceBreakdown)
     }, [query]);
