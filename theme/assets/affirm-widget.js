9340ff3d047b5cec5b2d65f62d130033fa8ef5af
hieu-huynh-emma <100395195+hieu-huynh-emma@users.noreply.github.com>
2024-10-28 11:53:11 +0700

    Affirm Dynamic Pricing (#438)
    * wip
    
    * wip
    
    * refactor: Upgrade 'Overlay' Module
    
    * feat: Extend 'Overlay' module to support multiple modal type
    
    * cleaning up
    
    * feat: Support Multiple Placement
    
    * apply AB Test
    
    * merge
    
    * feat: Add action tracker

--- theme/assets/affirm-widget.js
+++ theme/assets/affirm-widget.js
@@ -1,7 +1,129 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    // Enable Fancybox for any element with the `data-fancybox` attribute
-    Fancybox.bind('[data-fancybox]', {
-        // Your custom options
-    });
+if (!customElements.get("affirm-widget")) {
+  class AffirmWidget extends CustomElement {
+    props = {
+      page: "product",
+      placement: "", // promotion-line
+      amount: 0,
+    };
+    installmentCount = 12;
 
-})
+    initialized = false;
+
+    constructor() {
+      super();
+    }
+
+    render() {
+      if (!this.initialized) return;
+
+      const { placement } = this.props;
+
+      let template = "";
+
+      switch (placement) {
+        case "promotion-badge":
+          template = document.querySelector(`#affirm-promotion-badge-tpl`).innerHTML;
+          break;
+        case "promotion-line":
+          template = document.querySelector(`#affirm-promotion-line-tpl`).innerHTML;
+          break;
+      }
+
+      this.$el.html(template);
+    }
+
+    mounted() {
+      const { placement, amount, page } = this.props;
+
+      console.log(232);
+
+      switch (placement) {
+        case "promotion-badge":
+          switch (page) {
+            case "product":
+              window.addEventListener("productVariantChange", debounce(this.onVariantChange.bind(this), 100));
+              this.onVariantChange();
+              break;
+            case "cart":
+              this.setObserver("#cart-total");
+              this.onCartTotalChange();
+          }
+
+          break;
+        case "promotion-line":
+          this.refresh(amount);
+          break;
+      }
+    }
+
+    onVariantChange() {
+      const $productPrice = $("product-price");
+
+      const { price } = $productPrice.data();
+
+      this.refresh(price);
+    }
+
+    onCartTotalChange() {
+      const wrapperEl = document.querySelector("#cart-total");
+      const cartTotalEl = wrapperEl.querySelector(".af_new_price") || wrapperEl;
+      const rawTotal = cartTotalEl.textContent.replace(/[\$,]/g, "");
+      const cartTotal = parseFloat(rawTotal).toFixed(2);
+
+      this.refresh(cartTotal);
+    }
+
+    setObserver(promo) {
+      const targetNode = document.querySelector(promo);
+      const config = { attributes: true, childList: true, subtree: true };
+
+      const callback = (mutationsList) => {
+        for (let mutation of mutationsList) {
+          this.onCartTotalChange();
+        }
+      };
+
+      const observer = new MutationObserver(callback);
+      if (targetNode) observer.observe(targetNode, config);
+    }
+
+
+    refresh(price) {
+      if (price < 300) {
+        return;
+      }
+
+      if (!this.initialized) this.initialized = true;
+
+      this.render();
+
+      const installmentData = this.computeInstallment(price, this.installmentCount);
+
+      this.renderInstallmentAmount(installmentData.displayAmountInCurrency);
+    }
+
+    computeInstallment(price, terms) {
+      const rawAmount = price / terms;
+      const amount = parseFloat(rawAmount).toFixed(2);
+      const displayAmount = parseFloat(Math.ceil(rawAmount)).toFixed(2);
+      const displayAmountInCurrency = Currency.format(displayAmount);
+
+      return {
+        rawAmount,
+        amount,
+        displayAmount,
+        displayAmountInCurrency,
+      };
+    }
+
+    renderInstallmentAmount(amountText) {
+      const amountEl = this.querySelector(`[slot="installment-amount"]`);
+
+      setTimeout(() => {
+        amountEl.textContent = amountText;
+      }, 10);
+    }
+  }
+
+  customElements.define("affirm-widget", AffirmWidget);
+}
--- theme/assets/custom-elements.js
+++ theme/assets/custom-elements.js
@@ -1,317 +1,335 @@
 class CustomElement extends HTMLElement {
-  props = null
+	props = null
 
-  _data = null
-  get data() {
-    return this._data
-  }
+	_data = null
+	get data() {
+		return this._data
+	}
 
-  set data(newVal) {
-    this._data = newVal
-  }
+	set data(newVal) {
+		this._data = newVal
+	}
 
-  static get observedAttributes() {
-    return ['disabled', "data"];
-  }
+	static get observedAttributes() {
+		return ['disabled', "data"];
+	}
 
-  _value = null
-  get value() {
-    return this._value
-  }
+	_value = null
+	get value() {
+		return this._value
+	}
 
-  set value(newVal) {
-    if (newVal === this._value) return
-    this._value = newVal
+	set value(newVal) {
+		if (newVal === this._value) return
+		this._value = newVal
 
-    this.onValueChange(newVal)
-  }
+		this.onValueChange(newVal)
+	}
 
-  _loading = false;
-  get loading() {
-    return this._loading
-  }
+	_loading = false;
+	get loading() {
+		return this._loading
+	}
 
-  set loading(newVal) {
-    if (newVal === this._loading) return
-    this._loading = newVal
-    this.onLoad(newVal)
-  }
+	set loading(newVal) {
+		if (newVal === this._loading) return
+		this._loading = newVal
+		this.onLoad(newVal)
+	}
 
-  _disabled = false;
-  get disabled() {
-    return this._disabled
-  }
+	_disabled = false;
+	get disabled() {
+		return this._disabled
+	}
 
-  set disabled(isDisabled) {
-    if (isDisabled === this._disabled) return
-    this._disabled = isDisabled
-    this.onDisabledChange(isDisabled)
-  }
+	set disabled(isDisabled) {
+		if (isDisabled === this._disabled) return
+		this._disabled = isDisabled
+		this.onDisabledChange(isDisabled)
+	}
 
-  _readOnly = false;
-  get readOnly() {
-    return this._readOnly
-  }
+	_readOnly = false;
+	get readOnly() {
+		return this._readOnly
+	}
 
-  set readOnly(isReadOnly) {
-    if (isReadOnly === this._readOnly) return
-    this._readOnly = isReadOnly
-    this.onReadOnlyChanged(isReadOnly)
-  }
-
-
-  constructor() {
-    super();
-    this.$el = $(this);
-    this.setup()
-  }
-
-  connectedCallback() {
-    this.extractProps();
-    this.init();
-
-    this.beforeMount()
-    this.render();
-
-    setTimeout(() => {
-      $(this).ready(this.mounted.bind(this))
-    }, 0)
-  }
-
-  disconnectedCallback() {
-    this.mutationObserver?.disconnect()
-
-    this.onDestroy()
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    this[name] = this.parseProp(newValue)
-
-    this.render()
-  }
-
-  setup() {
-    this.mutationObserver = new MutationObserver(this.onMutation.bind(this));
-  }
-
-  init() {
-    this.$el.addClass(`${this.tagName.toLowerCase()}`)
-  }
-
-  extractProps() {
-    const isObject = typeof this.props === 'object' && this.props !== null
-
-    if (!isObject) return
-
-    this.props = Object.entries(this.props).reduce((r, [propName, defaultValue]) => {
-
-      const rawVal = this.getAttribute(`:${propName.toLowerCase()}`)
-
-      const propVal = this.parseProp(rawVal)
-
-      r[propName] = propVal || defaultValue
-
-      return r
-    }, {});
-
-    this.value = this.getAttribute(`:value`)
-
-    this.readOnly = this.hasAttribute('readOnly') || this.getAttribute('readOnly') === 'readOnly'
-  }
-
-  parseProp(rawVal) {
-    let val
-
-    try {
-      val = JSON.parse(rawVal);
-    } catch {
-      val = rawVal
-    }
-    return val
-  }
+	set readOnly(isReadOnly) {
+		if (isReadOnly === this._readOnly) return
+		this._readOnly = isReadOnly
+		this.onReadOnlyChanged(isReadOnly)
+	}
 
 
-  beforeMount() {
-  }
+	constructor() {
+		super();
+		this.$el = $(this);
+		this.setup()
+	}
 
-  template() {
+	connectedCallback() {
+		this.extractProps();
+		this.init();
 
-  }
+		this.beforeMount()
+		this.render();
 
-  render() {
-    if (this.template()) {
-      this.innerHTML = this.template();
+		setTimeout(() => {
+			$(this).ready(this.mounted.bind(this))
+		}, 0)
+	}
 
-      this.onUpdated();
-    }
-  }
+	disconnectedCallback() {
+		this.mutationObserver?.disconnect()
 
-  mounted() {
-  }
+		this.onDestroy()
+	}
 
-  onUpdated() {}
+	attributeChangedCallback(name, oldValue, newValue) {
+		this[name] = this.parseProp(newValue)
 
-  onMutation() {
-  }
+		this.render()
+	}
 
-  onDestroy() {
-  }
+	setup() {
+		this.mutationObserver = new MutationObserver(this.onMutation.bind(this));
+	}
 
-  onValueChange() {
-  }
+	init() {
+		this.$el.addClass(`${this.tagName.toLowerCase()}`)
+	}
 
-  onLoad(isLoading) {
-    this.$el[!!isLoading ? 'addClass' : 'removeClass']('is-loading')
+	extractProps() {
+		const isObject = typeof this.props === 'object' && this.props !== null
 
-    const firstLoadingOverlays = this.$el.find('loading-overlay').first().get(0)
+		if (!isObject) return
 
-    if (!!firstLoadingOverlays) {
-      firstLoadingOverlays[isLoading ? 'show' : 'hide']()
-    }
-  }
+		this.props = Object.entries(this.props).reduce((r, [propName, defaultValue]) => {
 
-  onDisabledChange(isDisabled) {
-    if (!!isDisabled) {
-      this.$el.attr('disabled', 'disabled')
-    } else {
-      this.$el.removeAttr('disabled')
-    }
-  }
+			const rawVal = this.getAttribute(`:${propName.toLowerCase()}`)
 
-  onReadOnlyChanged(isReadOnly) {
-    this.$el[!!isReadOnly ? 'attr' : 'removeAttr']('readOnly', 'readOnly')
-  }
+			const propVal = this.parseProp(rawVal)
+
+			r[propName] = propVal || defaultValue
+
+			return r
+		}, {});
+
+		this.value = this.getAttribute(`:value`)
+
+		this.readOnly = this.hasAttribute('readOnly') || this.getAttribute('readOnly') === 'readOnly'
+	}
+
+	parseProp(rawVal) {
+		let val
+
+		try {
+			val = JSON.parse(rawVal);
+		} catch {
+			val = rawVal
+		}
+		return val
+	}
+
+
+	beforeMount() {
+	}
+
+	template() {
+
+	}
+
+	render() {
+		if (this.template()) {
+			this.innerHTML = this.template();
+
+			this.onUpdated();
+		}
+	}
+
+	mounted() {
+	}
+
+	onUpdated() {}
+
+	onMutation() {
+	}
+
+	onDestroy() {
+	}
+
+	onValueChange() {
+	}
+
+	onLoad(isLoading) {
+		this.$el[!!isLoading ? 'addClass' : 'removeClass']('is-loading')
+
+		const firstLoadingOverlays = this.$el.find('loading-overlay').first().get(0)
+
+		if (!!firstLoadingOverlays) {
+			firstLoadingOverlays[isLoading ? 'show' : 'hide']()
+		}
+	}
+
+	onDisabledChange(isDisabled) {
+		if (!!isDisabled) {
+			this.$el.attr('disabled', 'disabled')
+		} else {
+			this.$el.removeAttr('disabled')
+		}
+	}
+
+	onReadOnlyChanged(isReadOnly) {
+		this.$el[!!isReadOnly ? 'attr' : 'removeAttr']('readOnly', 'readOnly')
+	}
 }
 
 class CustomButton extends CustomElement {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  setup() {
-    super.setup();
+	setup() {
+		super.setup();
 
-    this.addEventListener('click', (event) => {
-      event.preventDefault();
+		this.addEventListener('click', (event) => {
+			event.preventDefault();
 
-      if (this.disabled || this.readOnly) {
-        event.stopImmediatePropagation()
-        return
-      }
+			if (this.disabled || this.readOnly) {
+				event.stopImmediatePropagation()
+				return
+			}
 
-      this.onClick(event);
-    });
-  }
+			this.onClick(event);
+		});
 
-  onClick(e) {
-  }
+		this.$el.addClass("cursor-pointer")
+	}
+
+	onClick(e) {
+	}
 }
 
 class ResponsiveComponent extends CustomElement {
-  props = {}
+	props = {}
 
-  _isMobile = null;
-  get isMobile() {
-    return this._isMobile
-  }
+	_isMobile = null;
+	get isMobile() {
+		return this._isMobile
+	}
 
-  set isMobile(isMobile) {
-    if (isMobile === this._isMobile) return
-    this._isMobile = isMobile
+	set isMobile(isMobile) {
+		if (isMobile === this._isMobile) return
+		this._isMobile = isMobile
 
-    this.onLayoutModeChange(isMobile)
-  }
+		this.onLayoutModeChange(isMobile)
+	}
 
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
+	disconnectedCallback() {
+		super.disconnectedCallback();
 
-    this.resizeObserver?.disconnect()
-  }
-
-
-  setup() {
-    super.setup();
-
-    this.resizeObserver = new ResizeObserver(debounce(this.onScreenResize.bind(this), 500));
-
-    this.templates = [...this.querySelectorAll("template")].reduce((r, node) => {
-      r[node.getAttribute('name')] = node.content
-      return r
-    }, {})
-  }
-
-  beforeMount() {
-    super.beforeMount();
-
-    this.resizeObserver.observe(document.body);
-  }
+		this.resizeObserver?.disconnect()
+	}
 
 
-  render() {
-    if (this.isMobile === null) return
+	setup() {
+		super.setup();
 
-    const tpl = this.templates[this.isMobile ? 'mobile' : 'desktop']
+		this.resizeObserver = new ResizeObserver(debounce(this.onScreenResize.bind(this), 500));
 
-    this.$el.html(tpl.cloneNode(true))
-  }
+		this.templates = [...this.querySelectorAll("template")].reduce((r, node) => {
+			r[node.getAttribute('name')] = node.content
+			return r
+		}, {})
+	}
 
-  onScreenResize(entries) {
-    const [entry] = entries
+	beforeMount() {
+		super.beforeMount();
 
-    const screenWidth = entry.contentRect.width
+		this.resizeObserver.observe(document.body);
+	}
 
-    this.isMobile = screenWidth <= 769
-  }
 
-  onLayoutModeChange(isMobile) {
-    if (isMobile) {
-      this.$el.addClass('mobile').removeClass('desktop')
-    } else {
-      this.$el.removeClass('mobile').addClass('desktop')
-    }
+	render() {
+		if (this.isMobile === null) return
 
-    this.render();
-  }
+		const tpl = this.templates[this.isMobile ? 'mobile' : 'desktop']
+
+		this.$el.html(tpl.cloneNode(true))
+	}
+
+	onScreenResize(entries) {
+		const [entry] = entries
+
+		const screenWidth = entry.contentRect.width
+
+		this.isMobile = screenWidth <= 769
+	}
+
+	onLayoutModeChange(isMobile) {
+		if (isMobile) {
+			this.$el.addClass('mobile').removeClass('desktop')
+		} else {
+			this.$el.removeClass('mobile').addClass('desktop')
+		}
+
+		this.render();
+	}
 }
 
 customElements.define('responsive-component', ResponsiveComponent);
 
 
 class ShadowElement extends CustomElement {
-  template
+	template
 
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.attachShadow({ mode: 'open' });
-    this.$shEl = $(this.shadowRoot)
+	}
 
-  }
+	connectedCallback() {
+		this.extractProps();
+		this.init();
 
-  connectedCallback() {
-    this.extractProps();
-    this.init();
+		this.beforeMount()
+		this.render();
 
-    this.beforeMount()
-    this.render();
+		setTimeout(() => {
+			$(this).ready(this.mounted.bind(this))
+		}, 0)
+	}
 
-    setTimeout(() => {
-      $(this).ready(this.mounted.bind(this))
-    }, 0)
-  }
+	beforeMount() {
+		this.slots = this.$el.find("[slot]").toArray().reduce(function (r, slot, i) {
+			const $slot = $(slot)
+			const slotName = $slot.attr('slot')
 
-  render() {
-    if (!this.template) return
+			r[slotName] = $slot
+			return r
+		}, {});
+	}
 
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+	render() {
+		if (!this.template) return
 
-  }
+		this.$el.html(this.template.innerHTML);
 
-  beforeMount() {
-    super.beforeMount();
-  }
+		this.renderSlots();
+
+	}
+
+	renderSlots() {
+		Object.entries(this.slots).forEach(([name, $slot]) => {
+			const $outlet = this.$el.find(`slot[name="${name}"]`)
+
+			if ($outlet.length) {
+				$outlet.replaceWith($slot)
+			}
+		})
+	}
+
 }
