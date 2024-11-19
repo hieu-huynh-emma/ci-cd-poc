ab52885474c5f6ad42cc6963c388852dccf6542b
Hieu Huynh <trunghieu.huynh@emma-sleep.com>
2024-11-19 10:45:01 +0700

    feat: End Affirm A/B campaign

--- theme/assets/product-layout.js
+++ theme/assets/product-layout.js
@@ -1,119 +1,106 @@
 const schema = [
-	{
-		sectionId: "product-summary",
-	},
-	{
-		sectionId: "product-badges",
-	},
-	{
-		sectionId: "product-media",
-	},
-	{
-		sectionId: "overlay-text-on-image",
-	},
-	{
-		sectionId: "risk-reversal",
-	},
-	{
-		sectionId: "campaign-teaser",
-	},
-	{
-		sectionId: "product-unique-selling-points",
-	},
-	{
-		sectionId: "product-specifications",
-	},
-	{
-		sectionId: "bundle-box",
-		outletQuery: "#attribute-configurator",
-		insertPosition: "afterend",
-	},
-	{
-		sectionId: "bundle-cross-sell",
-		outletQuery: "#attribute-configurator",
-		insertPosition: "afterend",
-	},
-	{
-		sectionId: "product-auxiliary",
-		blocks: [
-			{
-				query: "cross-sell-panel",
-				outletQuery: "#attribute-configurator",
-				insertPosition: "afterend",
-			},
-			{
-				query: `product-auxiliary[name="upsell-widget"]`,
-				outletQuery: "#attribute-configurator",
-				insertPosition: "afterend",
-			},
-		],
-	},
-	{
-		sectionId: "price-transparency",
-		type: "clone",
-		blocks: [
-			{
-				query: "[abtasty-campaign-id=\"1317071\"][abtasty-campaign-variation=\"0\"]",
-				outletQuery: "product-price",
-				insertPosition: "afterend",
-			},
-			{
-				query: "[abtasty-campaign-id=\"1317071\"][abtasty-campaign-variation=\"1\"]",
-				outletQuery: ".installment-widgets",
-				insertPosition: "beforeend",
-			},
-		],
-	},
+    {
+        sectionId: "product-summary",
+    },
+    {
+        sectionId: "product-badges",
+    },
+    {
+        sectionId: "product-media",
+    },
+    {
+        sectionId: "overlay-text-on-image",
+    },
+    {
+        sectionId: "price-transparency"
+    },
+    {
+        sectionId: "risk-reversal",
+    },
+    {
+        sectionId: "campaign-teaser",
+    },
+    {
+        sectionId: "product-unique-selling-points",
+    },
+    {
+        sectionId: "product-specifications",
+    },
+    {
+        sectionId: "bundle-box",
+        outletQuery: "#attribute-configurator",
+        insertPosition: "afterend",
+    },
+    {
+        sectionId: "bundle-cross-sell",
+        outletQuery: "#attribute-configurator",
+        insertPosition: "afterend",
+    },
+    {
+        sectionId: "product-auxiliary",
+        blocks: [
+            {
+                query: "cross-sell-panel",
+                outletQuery: "#attribute-configurator",
+                insertPosition: "afterend",
+            },
+            {
+                query: `product-auxiliary[name="upsell-widget"]`,
+                outletQuery: "#attribute-configurator",
+                insertPosition: "afterend",
+            },
+        ],
+    }
 ];
 const sectionId = document.currentScript.dataset["sectionId"];
 const templateId = sectionId.split("__")[0];
 
 
 function loadSection({sectionId, type, outletQuery, insertPosition, blocks = []}) {
-	const sectionEl = document.querySelector(`#shopify-section-${templateId}__${sectionId}`);
+    const sectionEl = document.querySelector(`#shopify-section-${templateId}__${sectionId}`);
 
-	if (!sectionEl) return
+    if (!sectionEl) return
 
-	const processedEl = type === 'clone' ? sectionEl.cloneNode(true) : sectionEl.parentElement.removeChild(sectionEl);
+    const processedEl = type === 'clone' ? sectionEl.cloneNode(true) : sectionEl.parentElement.removeChild(sectionEl);
 
-	if (outletQuery) {
-		const outletEl = document.querySelector(outletQuery)
-		outletEl?.insertAdjacentElement(insertPosition, processedEl);
-	} else {
-		const sectionPlaceholderEl = document.querySelector(`#product-layout #${sectionId}-placeholder`);
-		sectionPlaceholderEl?.replaceWith(processedEl);
-	}
+    if (outletQuery) {
+        const outletEl = document.querySelector(outletQuery)
+        outletEl?.insertAdjacentElement(insertPosition, processedEl);
+    } else {
+        const sectionPlaceholderEl = document.querySelector(`#product-layout #${sectionId}-placeholder`);
+        sectionPlaceholderEl?.replaceWith(processedEl);
+    }
 
-	if (blocks.length > 0) {
-		blocks.forEach(({query, insertPosition, outletQuery}) => {
-			try {
-				const blockEl = sectionEl.querySelector(query);
-				if (!blockEl) return;
-				const detachedBlockEl = blockEl.parentElement.removeChild(blockEl);
-				const outletEl = document.querySelector(outletQuery);
+    if (blocks.length > 0) {
+        blocks.forEach(({query, insertPosition, outletQuery}) => {
+            try {
+                const blockEl = sectionEl.querySelector(query);
+                if (!blockEl) return;
+                const detachedBlockEl = blockEl.parentElement.removeChild(blockEl);
+                const outletEl = document.querySelector(outletQuery);
 
-				outletEl.insertAdjacentElement(insertPosition, detachedBlockEl);
-			} catch (e) {
-				console.error(e);
-			}
-		});
-	}
+                outletEl.insertAdjacentElement(insertPosition, detachedBlockEl);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+    }
 
-	sectionEl.classList.remove("hidden");
+    sectionEl.classList.remove("hidden");
 
-	setTimeout(() => {
-		sectionEl.classList.add("is-initialized");
-	}, 500);
+    setTimeout(() => {
+        sectionEl.classList.add("is-initialized");
+    }, 500);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-	schema.forEach(loadSection);
+    schema.forEach(loadSection);
 
-	$("#product-layout .skeleton").hide();
+    $("#product-layout .skeleton").hide();
 
-	const productLayoutEl = document.querySelector("#product-layout");
+    const productLayoutEl = document.querySelector("#product-layout");
 
-	productLayoutEl.classList.add("is-initialized");
+    productLayoutEl.classList.add("is-initialized");
 
 
 });
