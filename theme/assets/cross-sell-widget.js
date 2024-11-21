6f09f4acbe9861abbd784ed718575682508fad9c
Hieu Huynh <trunghieu.huynh@emma-sleep.com>
2024-11-21 14:34:37 +0700

    feat: Implement 'Cross Sell' for mattress trio

--- theme/assets/cross-sell-widget.js
+++ theme/assets/cross-sell-widget.js
@@ -1,34 +1,34 @@
 import CrossSell from "cross-sell";
-import { CROSS_SELLING_CHANGE_EVENT } from "data-constants";
+import {CROSS_SELLING_CHANGE_EVENT} from "data-constants";
 
 
 class CrossSellPanel extends CrossSell.Manager {
-  props = {
-    productId: 0,
-    variantId: 0,
-    trackId: "",
-    sizeCompatible: false,
-    mode: "Product",
-    switchable: false,
-  };
+    props = {
+        productId: 0,
+        variantId: 0,
+        trackId: "",
+        sizeCompatible: false,
+        mode: "Product",
+        switchable: false,
+    };
 
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  template() {
-    const { trackId, switchable } = this.props;
+    template() {
+        const {trackId, switchable} = this.props;
 
-    return this.crossSellProducts.reduce((r, metaobject) => {
-      const {
-        qty,
-        variant: { id, price, originalPrice, product },
-        parent,
-      } = metaobject;
+        return this.crossSellProducts.reduce((r, metaobject) => {
+            const {
+                qty,
+                variant: {id, price, originalPrice, product},
+                parent,
+            } = metaobject;
 
-      const { title, displayName, handle, featuredImage } = product;
+            const {title, displayName, handle, featuredImage} = product;
 
-      return r += `<cross-sell-widget
+            return r += `<cross-sell-widget
               name="cross-sell"
               type="widget"
               id="cross-sell-${handle}"
@@ -70,113 +70,118 @@ class CrossSellPanel extends CrossSell.Manager {
                         </div>
                    </tracked-button>  
             </cross-sell-widget>`;
-    }, ``);
-  }
+        }, ``);
+    }
 
-  beforeMount() {
-    const { mode } = this.props;
+    beforeMount() {
+        const {mode} = this.props;
 
-    (mode === "Variant") && this.listenToParent();
-  }
+        (mode === "Variant") && this.listenToParent();
+    }
 
-  mounted() {
-    super.mounted();
+    mounted() {
+        super.mounted();
 
-    const { variantId, mode, productId } = this.props;
+        const {variantId, mode, productId} = this.props;
 
-    const initialId = (mode === "Variant") ? variantId : productId;
+        const initialId = (mode === "Variant") ? variantId : productId;
 
-    this.onChange(initialId);
-  }
+        this.onChange(initialId);
+    }
 
-  async update(entityId) {
-    const { mode } = this.props;
-    const { entries } = (await ((mode === "Variant") ? CrossSell.API.perVariant(entityId) : CrossSell.API.perProduct(entityId)));
+    async update(entityId) {
+        const {mode} = this.props;
+        const {entries} = (await ((mode === "Variant") ? CrossSell.API.perVariant(entityId) : CrossSell.API.perProduct(entityId)));
 
-    this.crossSellProducts = entries;
-  }
+        if (!entries.length) {
+            this.destroy()
+            return
+        }
+
+        this.crossSellProducts = entries;
+    }
 }
 
 customElements.define("cross-sell-panel", CrossSellPanel);
 
 class CrossSellWidget extends ProductAuxiliary {
-  get refs() {
-    return {
-      $checkboxInput: this.$el.find(".widget-checkbox__input"),
-      $variantSelector: this.$el.find("cross-sell-variant-selector"),
-    };
-  }
+    get refs() {
+        return {
+            $checkboxInput: this.$el.find(".widget-checkbox__input"),
+            $variantSelector: this.$el.find("cross-sell-variant-selector"),
+        };
+    }
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.addEventListener("change", () => {
-      this.dispatchEvent(new Event(CROSS_SELLING_CHANGE_EVENT));
-      this.updateMetadata();
+        this.addEventListener("change", () => {
+            this.dispatchEvent(new Event(CROSS_SELLING_CHANGE_EVENT));
+            this.updateMetadata();
 
-      if (this.isIncluded) {
+            if (this.isIncluded) {
+                window.dispatchEvent(new Event("productPriceChange"));
+            }
+        });
+    }
+
+    onClick(e) {
+        super.onClick();
+
+        e.preventDefault();
+
+        const {$variantSelector, $checkboxInput} = this.refs;
+
+        if ($variantSelector.get(0).contains(e.target)) return;
+
+        this.isIncluded = !$checkboxInput.is(":checked");
+
+        $checkboxInput.prop("checked", this.isIncluded);
+
         window.dispatchEvent(new Event("productPriceChange"));
-      }
-    });
-  }
+    }
 
-  onClick(e) {
-    super.onClick();
+    updateMetadata() {
+        const {$variantSelector} = this.refs;
+        const $selectedOption = $variantSelector.get(0).getSelected();
 
-    e.preventDefault();
-
-    const { $variantSelector, $checkboxInput } = this.refs;
-
-    if ($variantSelector.get(0).contains(e.target)) return;
-
-    this.isIncluded = !$checkboxInput.is(":checked");
-
-    $checkboxInput.prop("checked", this.isIncluded);
-
-    window.dispatchEvent(new Event("productPriceChange"));
-  }
-
-  updateMetadata() {
-    const { $variantSelector } = this.refs;
-    const $selectedOption = $variantSelector.get(0).getSelected();
-
-    this.$el.data({
-      price: $selectedOption.data("price"),
-      originalPrice: $selectedOption.data("originalPrice"),
-      variantId: $selectedOption.val(),
-    });
-  }
+        this.$el.data({
+            price: $selectedOption.data("price"),
+            originalPrice: $selectedOption.data("originalPrice"),
+            variantId: $selectedOption.val(),
+        });
+    }
 }
 
 customElements.define("cross-sell-widget", CrossSellWidget);
 
 class CrossSellVariantSelector extends CustomElement {
-  product = {};
-  props = {
-    variantId: 0,
-  };
-
-  get refs() {
-    return {
-      $widget: this.$el.closest("cross-sell-widget"),
+    product = {};
+    props = {
+        variantId: 0,
     };
-  }
 
-  constructor() {
-    super();
+    get refs() {
+        return {
+            $widget: this.$el.closest("cross-sell-widget"),
+        };
+    }
 
-    this.product = JSON.parse(this.$el.closest("cross-sell-widget").find("script[type='application/json'][name='parent']").text());
-  }
+    constructor() {
+        super();
 
-  mounted() {
-    super.mounted();
+        this.product = JSON.parse(this.$el.closest("cross-sell-widget").find("script[type='application/json'][name='parent']").text());
+    }
 
-    this.dispatchEvent(new Event("change", { bubbles: true }));
-  }
+    mounted() {
+        super.mounted();
 
-  template() {
-    const { product } = this;
-    return `
+        this.dispatchEvent(new Event("change", {bubbles: true}));
+    }
+
+    template() {
+        const {product} = this;
+        return `
        <select
           id="cross-sell-product-select-{{ product.id }}"
           data-id="{{ product.id }}"
@@ -184,13 +189,13 @@ class CrossSellVariantSelector extends CustomElement {
             ${product.variants.map(this.renderVariantSelectorOption.bind(this)).join("")}
         </select>
     `;
-  }
+    }
 
-  renderVariantSelectorOption(v) {
-    const { variantId } = this.props;
-    if (!v.available) return `<option disabled="disabled">${v.title} - Sold out</option>`;
+    renderVariantSelectorOption(v) {
+        const {variantId} = this.props;
+        if (!v.available) return `<option disabled="disabled">${v.title} - Sold out</option>`;
 
-    return `<option
+        return `<option
         ${+variantId === +v.id ? "selected" : ""}
         data-sku="${v.sku}"
         data-price="${v.price}"
@@ -199,60 +204,60 @@ class CrossSellVariantSelector extends CustomElement {
       >
         ${v.title}
       </option>`;
-  }
+    }
 
-  getSelected() {
-    return this.$el.find(":selected");
-  }
+    getSelected() {
+        return this.$el.find(":selected");
+    }
 }
 
 customElements.define("cross-sell-variant-selector", CrossSellVariantSelector);
 
 class CrossSellPrice extends CustomElement {
-  get refs() {
-    const $widget = this.$el.closest("cross-sell-widget");
-    return {
-      $widget,
-      $variantSelector: $widget.find("cross-sell-variant-selector"),
-    };
-  }
+    get refs() {
+        const $widget = this.$el.closest("cross-sell-widget");
+        return {
+            $widget,
+            $variantSelector: $widget.find("cross-sell-variant-selector"),
+        };
+    }
 
-  pricing = { price, originalPrice };
+    pricing = {price, originalPrice};
 
-  constructor() {
-    super();
-    const { $widget } = this.refs;
-    this.variant = JSON.parse(this.$el.closest("cross-sell-widget").find("script[type='application/json'][name='variant']").text());
+    constructor() {
+        super();
+        const {$widget} = this.refs;
+        this.variant = JSON.parse(this.$el.closest("cross-sell-widget").find("script[type='application/json'][name='variant']").text());
 
-    $widget.on(CROSS_SELLING_CHANGE_EVENT, this.updatePrice.bind(this));
-  }
+        $widget.on(CROSS_SELLING_CHANGE_EVENT, this.updatePrice.bind(this));
+    }
 
-  template() {
-    const { price, originalPrice } = this.pricing;
+    template() {
+        const {price, originalPrice} = this.pricing;
 
-    const totalSaved = Math.max(0, originalPrice - price),
-      priceInCurrency = Currency.format(parseFloat(price)),
-      originalPriceInCurrency = Currency.format(parseFloat(originalPrice));
+        const totalSaved = Math.max(0, originalPrice - price),
+            priceInCurrency = Currency.format(parseFloat(price)),
+            originalPriceInCurrency = Currency.format(parseFloat(originalPrice));
 
-    return `
+        return `
         <p class="product-price-container">
             <span class="text-scarlet font-bold">${priceInCurrency}</span>
             ${totalSaved ? `<del class="line-through text-xs">${originalPriceInCurrency}</del>` : ""}
         </p>
     `;
-  }
+    }
 
-  updatePrice() {
-    const { $variantSelector } = this.refs;
-    const $selectedOption = $variantSelector.get(0).getSelected();
+    updatePrice() {
+        const {$variantSelector} = this.refs;
+        const $selectedOption = $variantSelector.get(0).getSelected();
 
-    this.pricing = {
-      price: $selectedOption.data("price"),
-      originalPrice: $selectedOption.data("originalPrice"),
-    };
+        this.pricing = {
+            price: $selectedOption.data("price"),
+            originalPrice: $selectedOption.data("originalPrice"),
+        };
 
-    this.render();
-  }
+        this.render();
+    }
 }
 
 customElements.define("cross-sell-price", CrossSellPrice);
